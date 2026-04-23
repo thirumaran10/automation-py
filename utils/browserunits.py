3f9ae6702ab4740c8c6cd28d6675be300a142618
ThirumaranM <thirumaranm10@gmail.com>
2026-04-23 21:53:32 +0530

    Update browserunits.py

--- utils/browserunits.py
+++ utils/browserunits.py
@@ -5,6 +5,7 @@ class browserutils:
     def __init__(self):
         self.driver = driver
 
+    
     # want to apply a logic on particular page there is no point to write a header footer in every page, reduce a code
     def gettitle(self):
         return self.driver.title
