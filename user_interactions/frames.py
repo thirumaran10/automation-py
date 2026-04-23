89bc3efd89fa4a7edaeedc51269e6a10f2d013f1
ThirumaranM <thirumaranm10@gmail.com>
2026-04-23 21:54:42 +0530

    Update frames.py

--- user_interactions/frames.py
+++ user_interactions/frames.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 import time
 from selenium.webdriver.common.by import By
 
+
 driver = webdriver.Chrome()
 driver.get("https://the-internet.herokuapp.com/iframe")
 driver.implicitly_wait(5)
