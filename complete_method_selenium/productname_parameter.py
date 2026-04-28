797b22317b8b97abcd77f2777e29ac02f9e02aea
ThirumaranM <thirumaranm10@gmail.com>
2026-04-28 21:03:58 +0530

    Update productname_parameter.py

--- complete_method_selenium/productname_parameter.py
+++ complete_method_selenium/productname_parameter.py
@@ -6,6 +6,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC 
 
+
 driver = webdriver.Chrome()
 driver.get("https://rahulshettyacademy.com/angularpractice/")
 driver.implicitly_wait(5)
